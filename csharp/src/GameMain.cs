f96b849a505ab3de9327534f6a403f33f7f23066
MobinAliMomin <mobinali1996@gmail.com>
2018-10-05 10:33:51 +1000

    add main file

--- csharp/src/GameMain.cs
+++ csharp/src/GameMain.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using SwinGameSDK;
 using static SwinGameSDK.SwinGame; // requires mcs version 4+, 
 // using SwinGameSDK.SwinGame; // requires mcs version 4+, 
@@ -12,17 +12,17 @@ namespace MyGame
             //Open the game window
             OpenGraphicsWindow("GameMain", 800, 600);
             ShowSwinGameSplashScreen();
-            
+
             //Run the game loop
-            while(false == WindowCloseRequested())
+            while (false == WindowCloseRequested())
             {
                 //Fetch the next batch of UI interaction
                 ProcessEvents();
-                
+
                 //Clear the screen and draw the framerate
                 ClearScreen(Color.White);
-                DrawFramerate(0,0);
-                
+                DrawFramerate(0, 0);
+
                 //Draw onto the screen
                 RefreshScreen(60);
             }
