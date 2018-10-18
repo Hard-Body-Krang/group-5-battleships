78483148b5e499df81748c9aae667174d6d9f094
MobinAliMomin <mobinali1996@gmail.com>
2018-10-19 08:57:40 +1100

    Improve code

--- csharp/src/DiscoveryController.cs
+++ csharp/src/DiscoveryController.cs
@@ -1,80 +1,69 @@
-
-using Microsoft.VisualBasic;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-//using System.Data;
-using System.Diagnostics;
 using SwinGameSDK;
+using System;
+using System.Collections.Generic;
 
-/// <summary>
-/// The battle phase is handled by the DiscoveryController.
-/// </summary>
-static class DiscoveryController
-{
+//  <summary>
+//  The battle phase is handled by the DiscoveryController.
+//  </summary>
+class DiscoveryController {
+   
+    //  <summary>
+    //  Handles input during the discovery phase of the game.
+    //  </summary>
+    //  <remarks>
+    //  Escape opens the game menu. Clicking the mouse will
+    //  attack a location.
+    //  </remarks>
+    public static void HandleDiscoveryInput() {
+        if (SwinGame.KeyTyped(KeyCode.EscapeKey)) { // condition if esc key is pressed
+            GameController.AddNewState(GameState.ViewingGameMenu);
+        }
+        
+        if (SwinGame.MouseClicked(MouseButton.LeftButton)) { // condition if m1 key is pressed
+            DiscoveryController.DoAttack();
+        }
+        
+    }
+    
+    //  <summary>
+    //  Attack the location that the mouse if over.
+    //  </summary>
+    private static void DoAttack() {
+        Point2D mouse = default(Point2D);
+        mouse = SwinGame.MousePosition();
+        // Calculate the row/col clicked
 
-	/// <summary>
-	/// Handles input during the discovery phase of the game.
-	/// </summary>
-	/// <remarks>
-	/// Escape opens the game menu. Clicking the mouse will
-	/// attack a location.
-	/// </remarks>
-	public static void HandleDiscoveryInput()
-	{
-		if (SwinGame.KeyTyped(KeyCode.vk_ESCAPE)) {
-			GameController.AddNewState(GameState.ViewingGameMenu);
-		}
+        int row = Convert.ToInt32(Math.Floor(((mouse.Y - UtilityFunctions.FIELD_TOP) / (UtilityFunctions.CELL_HEIGHT + UtilityFunctions.CELL_GAP)))); // draw grid row
+        int col = Convert.ToInt32(Math.Floor(((mouse.X - UtilityFunctions.FIELD_LEFT) / (UtilityFunctions.CELL_WIDTH + UtilityFunctions.CELL_GAP)))); // draw grid column
+        if (((row >= 0) && (row < GameController.HumanPlayer.EnemyGrid.Height))) { // check if its a valid space to do attack
+            if (((col >= 0) && (col < GameController.HumanPlayer.EnemyGrid.Width))) {
+                GameController.Attack(row, col); //  commence  attack
+            }
+        }
+    }
+    
+    //  <summary>
+    //  Draws the game during the attack phase.
+    //  </summary>s
+    public static void DrawDiscovery() {
+        const int SCORES_LEFT = 172;
+        const int SHOTS_TOP = 157;
+        const int HITS_TOP = 206;
+        const int SPLASH_TOP = 256;
+        if ((SwinGame.KeyDown(KeyCode.LeftShiftKey) || SwinGame.KeyDown(KeyCode.RightShiftKey)) && SwinGame.KeyDown(KeyCode.CKey)) 
+        {
+            // TODO - understand what this does
+            UtilityFunctions.DrawField(GameController.HumanPlayer.EnemyGrid, GameController.ComputerPlayer, true); // This is what happen if enemy's ship discovered
+            // BUG - possible bug as I believe that ships should be displayed
+        }
+        else {
+            UtilityFunctions.DrawField(GameController.HumanPlayer.EnemyGrid, GameController.ComputerPlayer, false); // this is when you fails to discover enemy ship
+        }
 
-		if (SwinGame.MouseClicked(MouseButton.LeftButton)) {
-			DoAttack();
-		}
-	}
-
-	/// <summary>
-	/// Attack the location that the mouse if over.
-	/// </summary>
-	private static void DoAttack()
-	{
-		Point2D mouse = default(Point2D);
-
-		mouse = SwinGame.MousePosition();
-
-		//Calculate the row/col clicked
-		int row = 0;
-		int col = 0;
-		row = Convert.ToInt32(Math.Floor((mouse.Y - UtilityFunctions.FIELD_TOP) / (UtilityFunctions.CELL_HEIGHT + UtilityFunctions.CELL_GAP)));
-		col = Convert.ToInt32(Math.Floor((mouse.X - UtilityFunctions.FIELD_LEFT) / (UtilityFunctions.CELL_WIDTH + UtilityFunctions.CELL_GAP)));
-
-		if (row >= 0 & row < GameController.HumanPlayer.EnemyGrid.Height) {
-			if (col >= 0 & col < GameController.HumanPlayer.EnemyGrid.Width) {
-				GameController.Attack(row, col);
-			}
-		}
-	}
-
-	/// <summary>
-	/// Draws the game during the attack phase.
-	/// </summary>s
-	public static void DrawDiscovery()
-	{
-		const int SCORES_LEFT = 172;
-		const int SHOTS_TOP = 157;
-		const int HITS_TOP = 206;
-		const int SPLASH_TOP = 256;
-
-		if ((SwinGame.KeyDown(KeyCode.vk_LSHIFT) | SwinGame.KeyDown(KeyCode.vk_RSHIFT)) & SwinGame.KeyDown(KeyCode.vk_c)) {
-			UtilityFunctions.DrawField(GameController.HumanPlayer.EnemyGrid, GameController.ComputerPlayer, true);
-		} else {
-			UtilityFunctions.DrawField(GameController.HumanPlayer.EnemyGrid, GameController.ComputerPlayer, false);
-		}
-
-		UtilityFunctions.DrawSmallField(GameController.HumanPlayer.PlayerGrid, GameController.HumanPlayer);
-		UtilityFunctions.DrawMessage();
-
-		SwinGame.DrawText(GameController.HumanPlayer.Shots.ToString(), Color.White, GameResources.GameFont("Menu"), SCORES_LEFT, SHOTS_TOP);
-		SwinGame.DrawText(GameController.HumanPlayer.Hits.ToString(), Color.White, GameResources.GameFont("Menu"), SCORES_LEFT, HITS_TOP);
-		SwinGame.DrawText(GameController.HumanPlayer.Missed.ToString(), Color.White, GameResources.GameFont("Menu"), SCORES_LEFT, SPLASH_TOP);
-	}
-
+        UtilityFunctions.DrawSmallField(GameController.HumanPlayer.PlayerGrid, GameController.HumanPlayer);
+        UtilityFunctions.DrawMessage(); // draw message
+        SwinGame.DrawText(GameController.HumanPlayer.Shots.ToString(), Color.White, GameResources.GameFont("Menu"), SCORES_LEFT, SHOTS_TOP);
+        SwinGame.DrawText(GameController.HumanPlayer.Hits.ToString(), Color.White, GameResources.GameFont("Menu"), SCORES_LEFT, HITS_TOP);
+        SwinGame.DrawText(GameController.HumanPlayer.Missed.ToString(), Color.White, GameResources.GameFont("Menu"), SCORES_LEFT, SPLASH_TOP);
+    }
 }
--- csharp/src/Model/Direction.cs
+++ csharp/src/Model/Direction.cs
@@ -1,20 +1,15 @@
-using Microsoft.VisualBasic;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-/// <summary>
-/// The direction the ship can be oriented.
-/// </summary>
-public enum Direction
-{
-	/// <summary>
-	/// The ship is oriented left/right
-	/// </summary>
-	LeftRight,
-
-	/// <summary>
-	/// The ship is oriented up/down
-	/// </summary>
-	UpDown
+// '' <summary>
+// '' The direction the ship can be oriented.
+// '' </summary>
+public enum Direction {
+    
+    // '' <summary>
+    // '' The ship is oriented left/right
+    // '' </summary>
+    LeftRight,
+    
+    // '' <summary>
+    // '' The ship is oriented up/down
+    // '' </summary>
+    UpDown,
 }
